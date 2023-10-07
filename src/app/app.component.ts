c835cd1bd803d31f399de155e2b47c08e5e38e39
Kolo <Kolo@DESKTOP-HLM3MBH>
2023-10-07 13:22:39 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,104 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-strength';
+  password = '';
+
+  onInput(event: Event) {
+    this.password = (event.target as HTMLTextAreaElement).value;
+  }
+
+  
+  getFirstColor(): string{
+    if(this.password.length == 0){
+      return "gray";
+    }
+  
+    if(this.password.length < 8 || this.isPasswordEasy()){
+      return "red";
+    }
+    
+    if(this.isPasswordMedium()){
+      return "yellow";
+    } 
+    
+    if(this.isPasswordStrong()){
+      return "green";
+    }
+  
+    return "gray";
+  } 
+  
+  getSecondColor(): string{
+    if(this.password.length == 0){
+      return "gray";
+    }
+  
+    if(this.password.length < 8){
+      return "red";
+    }
+  
+    if(this.isPasswordEasy()){
+      return "gray";
+    }
+    
+    if(this.isPasswordMedium()){
+      return "yellow";
+    } 
+    
+    if(this.isPasswordStrong()){
+      return "green";
+    }
+  
+    return "gray";
+  }
+  
+  getThirdColor(): string{
+  
+    if(this.password.length == 0){
+      return "gray";
+    }
+  
+    if(this.password.length < 8){
+      return "red";
+    }
+  
+    
+    if(this.isPasswordEasy()){
+      return "gray";
+    }
+    
+    
+    if(this.isPasswordMedium()){
+      return "gray";
+    } 
+    
+    if(this.isPasswordStrong()){
+      return "green";
+    }
+  
+    return "gray";
+  }
+  
+  isPasswordEasy(): boolean{
+     const easyRegex = /^[a-zA-Z]+$|^[0-9]+$|^[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]+$/;
+     const mediumRegex = /^(?=.*[a-zA-Z])(?=.*[\W_]).+$|^(?=.*[a-zA-Z])(?=.*\d).+$|^(?=.*\d)(?=.*[\W_]).+$/;
+     const strongRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/;
+     
+     return easyRegex.test(this.password) && !mediumRegex.test(this.password) && !strongRegex.test(this.password);
+  }
+  
+  isPasswordMedium(): boolean{
+     const mediumRegex = /^(?=.*[a-zA-Z])(?=.*[\W_]).+$|^(?=.*[a-zA-Z])(?=.*\d).+$|^(?=.*\d)(?=.*[\W_]).+$/;
+     const strongRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/;
+     
+     return mediumRegex.test(this.password) && !strongRegex.test(this.password);
+  }
+  
+  isPasswordStrong(): boolean{
+     const strongRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$/;
+     
+     return strongRegex.test(this.password);
+  }
+  
+
 }
